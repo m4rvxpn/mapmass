912d40117e714923eb3b6b9ccd0b05dd320cf2fe
m4rvxpn <clintjos99@gmail.com>
2023-02-24 13:49:46 +0530

    Create mapmss.py
    mapmass is a Python script that automates the process of scanning targets using Masscan and Nmap. Masscan is a high-speed port scanner that can scan the entire internet in under 6 minutes, while Nmap is a popular and powerful network exploration and security auditing tool.
    
    This script allows users to specify one or more targets, which can be in the form of CIDR ranges, IP addresses, or domain names. The script then uses Masscan to scan for open ports on the specified targets, and uses the open ports found to run Nmap scans against the targets.
    
    Users can customize the Masscan and Nmap commands with various options and flags, and can specify an output directory for the scan results. The script saves Masscan and Nmap scan results in separate folders within the output directory.
    
    This tool can be useful for network administrators, security professionals, and penetration testers who need to quickly and efficiently scan for open ports and identify potential vulnerabilities on large numbers of targets.

---
+++ mapmss.py
@@ -0,0 +1,43 @@
+import argparse
+import subprocess
+import os
+
+# Define argument parser
+parser = argparse.ArgumentParser(description='Run Masscan and Nmap scans against CIDR, IP, or domain targets.')
+parser.add_argument('target', metavar='target', type=str, help='the target(s) to scan, separated by commas')
+parser.add_argument('--masscan-args', dest='masscan_args', metavar='args', type=str, default='-p1-65535', help='additional arguments to pass to Masscan (default: -p1-65535)')
+parser.add_argument('--nmap-args', dest='nmap_args', metavar='args', type=str, default='-sV', help='additional arguments to pass to Nmap (default: -sV)')
+parser.add_argument('--output-dir', dest='output_dir', metavar='dir', type=str, default='results', help='directory to save scan results (default: results)')
+
+args = parser.parse_args()
+
+# Create output directory
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
+
+# Split targets string into list
+targets = args.target.split(',')
+
+# Run Masscan scans
+for target in targets:
+    masscan_cmd = f'masscan {target} {args.masscan_args} --rate=10000 -oG {args.output_dir}/{target}.gnmap'
+    subprocess.run(masscan_cmd, shell=True)
+    print(f'Masscan scan for {target} complete.')
+
+# Run Nmap scans on open ports
+for target in targets:
+    open_ports = []
+    with open(f'{args.output_dir}/{target}.gnmap') as f:
+        for line in f:
+            if 'Ports:' in line:
+                ports_str = line.strip().split(':')[1]
+                ports_list = ports_str.split(',')
+                for port in ports_list:
+                    port_num = port.split('/')[0]
+                    open_ports.append(port_num)
+    
+    open_ports_str = ','.join(open_ports)
+    
+    nmap_cmd = f'nmap -p{open_ports_str} {args.nmap_args} {target} -oA {args.output_dir}/{target}_nmap'
+    subprocess.run(nmap_cmd, shell=True)
+    print(f'Nmap scan for {target} complete.')
